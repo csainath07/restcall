fdebd4029d23a87f893cae2ad7aa0608c1b23a03
Sainath <servify@SL-SP-LAP-022.local>
2020-02-09 09:39:58 +0530

    bugs resolved

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
 ## RestCall
+
 RestCall allows you execute REST APIs and view it's response in Visual Studio Code directly
 
 ## Usage
 
 ```
-// GET https://jsonplaceholder.typicode.com/posts ${HEADER_OBJECT} ${BODY_OBJECT} {EXTRACT_KEY}
+// GET=>https://jsonplaceholder.typicode.com/posts=>${HEADER_OBJECT}=>${BODY_OBJECT}=>{EXTRACT_KEY}
 ```
-> `HEADER_OBJECT` and `BODY_OBJECT` must be `inline` and without `space`
 
-* `//` Single line comment, to avoid syntax error (**required**)
-* `GET` Http methods, it must be `GET`, `POST`, `PUT` or `DELETE` (**required**)
-* `${END_POINT}` absolute path of API endpoint (**required**)
-* `${HEADER_OBJECT}` headers config object (**optional**)
-* `${BODY}` request body object (**optional**)
-* `${EXTRACT_KEY}` extarct key from response object, **e.g** data (i.e show only data property from response) (**optional**)
+> `HEADER_OBJECT` and `BODY_OBJECT` must be `inline` and without `space` and separated by `=>`
+
+- `//` Single line comment, to avoid syntax error (**required**)
+- `GET` Http methods, it must be `GET`, `POST`, `PUT` or `DELETE` (**required**)
+- `${END_POINT}` absolute path of API endpoint (**required**)
+- `${HEADER_OBJECT}` headers config object (**optional**)
+- `${BODY}` request body object (**optional**)
+- `${EXTRACT_KEY}` extarct key from response object, **e.g** data (i.e show only data property from response) (**optional**)
 
 > Put cursor to rest command and press `cmd + shift + P` to open command prompt
 
--- extension.js
+++ extension.js
@@ -1,9 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-const fs = require('fs');
-const axios = require('axios').default;
-const util = require('util');
+const vscode = require("vscode");
+const fs = require("fs");
+const axios = require("axios").default;
+const util = require("util");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,71 +12,85 @@ const util = require('util');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	const HTTPMethods = ['GET', 'POST', 'PUT', 'DELETE'];
+  const HTTPMethods = ["GET", "POST", "PUT", "DELETE"];
 
-	let disposable = vscode.commands.registerTextEditorCommand('extension.restcall', function (editor) {
-		// Get active line and it's content
-		const activeLine = editor.selection.active.line;
-		let lineContent = editor.document.lineAt(activeLine)['_text'];
-		lineContent = lineContent.trim();
-		fs.writeFileSync(`${__dirname}/Response.json`, 'Loading...');
-		
-		// check content must be in valid format
-		if (lineContent !== '') {
-			const HTTPRequest = lineContent.split(" "); // Get all parameters
+  let disposable = vscode.commands.registerTextEditorCommand(
+    "extension.restcall",
+    function(editor) {
+      // Get active line and it's content
+      const activeLine = editor.selection.active.line;
+      let lineContent = editor.document.lineAt(activeLine)["_text"];
+      lineContent = lineContent.trim();
+      fs.writeFileSync(`${__dirname}/Response.json`, "Loading...");
 
-			if (HTTPRequest.length >= 3) { // Content atleast have HTTP method and URI `// {HTTP_METHOD} {URI}`
-				const givenHTTPMethod = HTTPRequest[1];
-				const givenEndPoint = HTTPRequest[2];
-				let givenHeader = HTTPRequest[3] || null;
-				let givenBody = HTTPRequest[4] || null;
-				const givenOnlyKey = HTTPRequest[5] || null;
+      // check content must be in valid format
+      if (lineContent !== "") {
+        let HTTPRequest = lineContent.slice(2);
+        HTTPRequest = HTTPRequest.split("=>"); // Get all parameters
 
-				const index = HTTPMethods.indexOf(givenHTTPMethod);
-				
-				// API config object
-				const configObj = {
-					method: givenHTTPMethod,
-					url: givenEndPoint,
-				};
-				
-				// set Header
-				givenHeader = JSON.parse(givenHeader);
-				configObj.headers = givenHeader || {};
+        if (HTTPRequest.length >= 2) {
+          // Content atleast have HTTP method and URI `// {HTTP_METHOD} {URI}`
+          const givenHTTPMethod = HTTPRequest[0].trim();
+          const givenEndPoint = HTTPRequest[1].trim();
+          let givenHeader = HTTPRequest[2].trim() || null;
+          let givenBody = HTTPRequest[3].trim() || null;
+          const givenOnlyKey = HTTPRequest[4].trim() || null;
 
-				// set body
-				givenBody = JSON.parse(givenBody);
-				configObj.data = givenBody || {}
+          const index = HTTPMethods.indexOf(givenHTTPMethod);
 
-				if (index !== -1) {
-					vscode.workspace.openTextDocument(`${__dirname}/Response.json`)
-						.then(doc =>  {
-							vscode.window.showTextDocument(doc, { viewColumn: vscode.ViewColumn.Beside })
-							return axios(configObj);
-						})
-						.then(resp => {
-							let json = givenOnlyKey ? util.inspect(resp[givenOnlyKey]) : util.inspect(resp);
-							fs.writeFileSync(`${__dirname}/Response.json`, json);
-						});
-				} else {
-					vscode.window.showErrorMessage(`Please select valid HTTP Method ['GET', 'POST', 'PUT', 'DELETE']`);
-				}
-			} else {
-				vscode.window.showErrorMessage('Content atleast have HTTP Method and URI `// {HTTP_METHOD} {URI}`');
-			}
-		} else {
-			vscode.window.showErrorMessage('Please select proper REST endpoint');
-		}
-	});
+          // API config object
+          const configObj = {
+            method: givenHTTPMethod,
+            url: givenEndPoint
+          };
 
-	context.subscriptions.push(disposable);
+          // set Header
+          givenHeader = JSON.parse(givenHeader);
+          configObj.headers = givenHeader || {};
+
+          // set body
+          givenBody = JSON.parse(givenBody);
+          configObj.data = givenBody || {};
+
+          if (index !== -1) {
+            vscode.workspace
+              .openTextDocument(`${__dirname}/Response.json`)
+              .then(doc => {
+                vscode.window.showTextDocument(doc, {
+                  viewColumn: vscode.ViewColumn.Beside
+                });
+                return axios(configObj);
+              })
+              .then(resp => {
+                let json = givenOnlyKey
+                  ? util.inspect(resp[givenOnlyKey])
+                  : util.inspect(resp);
+                fs.writeFileSync(`${__dirname}/Response.json`, json);
+              });
+          } else {
+            vscode.window.showErrorMessage(
+              `Please select valid HTTP Method ['GET', 'POST', 'PUT', 'DELETE']`
+            );
+          }
+        } else {
+          vscode.window.showErrorMessage(
+            "Content atleast have HTTP Method and URI `// {HTTP_METHOD} {URI}`"
+          );
+        }
+      } else {
+        vscode.window.showErrorMessage("Please select proper REST endpoint");
+      }
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() { }
+function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate
+};
